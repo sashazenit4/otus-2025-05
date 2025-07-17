34ce6ab69e44cb5959f6c3f91fa876d82922ebcd
Aleksandr Kholin <holinshura@gmail.com>
2025-07-17 21:22:25 +0300

    feat(excel-export): implement excel export in component

---
+++ local/components/otus/book.grid/lang/ru/class.php
@@ -0,0 +1,7 @@
+<?php
+$MESS['BOOK_GRID_BOOK_TITLE_LABEL'] = 'Название книги';
+$MESS['BOOK_GRID_BOOK_PUBLISHING_YEAR_LABEL'] = 'Год публикации';
+$MESS['BOOK_GRID_BOOK_PAGES_LABEL'] = 'Количество страниц';
+$MESS['BOOK_GRID_BOOK_AUTHORS_LABEL'] = 'Авторы';
+$MESS['BOOK_GRID_BOOK_PUBLISHING_DATE_LABEL'] = 'Дата публикации';
+$MESS['EXPORT_XLSX_BUTTON_TITLE'] = 'Выгрузка в Excel';
